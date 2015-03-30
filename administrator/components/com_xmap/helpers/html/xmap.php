f27921232fba2eae946b88aa1be13aed3a7f8c87
jui <jui@Josef>
2015-03-30 14:36:24 +0200

    Change class name JHTML to JHtml

--- administrator/components/com_xmap/helpers/html/xmap.php
+++ administrator/components/com_xmap/helpers/html/xmap.php
@@ -1,35 +1,34 @@
 <?php
+
 /**
  * @version     $Id$
  * @copyright   Copyright (C) 2007 - 2009 Joomla! Vargas. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  * @author      Guillermo Vargas (guille@vargas.co.cr)
  */
-
 // no direct access
 defined('_JEXEC') or die;
 
-JTable::addIncludePath( JPATH_COMPONENT . '/tables' );
+JTable::addIncludePath(JPATH_COMPONENT . '/tables');
 
 /**
  * @package       Xmap
  * @subpackage    com_xmap
  */
-abstract class JHtmlXmap
-{
+abstract class JHtmlXmap {
 
     /**
      * @param    string  $name
      * @param    string  $value
      * @param    int     $j
      */
-    public static function priorities($name, $value = '0.5', $j)
-    {
+    public static function priorities($name, $value = '0.5', $j) {
         // Array of options
-        for ($i=0.1; $i<=1;$i+=0.1) {
-            $options[] = JHTML::_('select.option',$i,$i);;
+        for ($i = 0.1; $i <= 1; $i+=0.1) {
+            $options[] = JHtml::_('select.option', $i, $i);
+            ;
         }
-        return JHtml::_('select.genericlist', $options, $name, null, 'value', 'text', $value, $name.$j);
+        return JHtml::_('select.genericlist', $options, $name, null, 'value', 'text', $value, $name . $j);
     }
 
     /**
@@ -37,16 +36,15 @@ abstract class JHtmlXmap
      * @param    string  $value
      * @param    int     $j
      */
-    public static function changefrequency($name, $value = 'weekly', $j)
-    {
+    public static function changefrequency($name, $value = 'weekly', $j) {
         // Array of options
-        $options[] = JHTML::_('select.option','hourly','hourly');
-        $options[] = JHTML::_('select.option','daily','daily');
-        $options[] = JHTML::_('select.option','weekly','weekly');
-        $options[] = JHTML::_('select.option','monthly','monthly');
-        $options[] = JHTML::_('select.option','yearly','yearly');
-        $options[] = JHTML::_('select.option','never','never');
-        return JHtml::_('select.genericlist', $options, $name, null, 'value', 'text', $value, $name.$j);
+        $options[] = JHtml::_('select.option', 'hourly', 'hourly');
+        $options[] = JHtml::_('select.option', 'daily', 'daily');
+        $options[] = JHtml::_('select.option', 'weekly', 'weekly');
+        $options[] = JHtml::_('select.option', 'monthly', 'monthly');
+        $options[] = JHtml::_('select.option', 'yearly', 'yearly');
+        $options[] = JHtml::_('select.option', 'never', 'never');
+        return JHtml::_('select.genericlist', $options, $name, null, 'value', 'text', $value, $name . $j);
     }
 
 }
